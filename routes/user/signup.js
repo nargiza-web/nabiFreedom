13d903c4a774fe01399ca4523ade3080bed9fed0
Ephriam Henderson <orik162@gmail.com>
2019-10-21 16:32:53 -0500

    [WIP]

--- routes/user/signup.js
+++ routes/user/signup.js
@@ -1,16 +1,45 @@
 const express = require("express")
 const router = express.Router()
 const bcrpyt = require("bcrypt")
+const models = require("../../models")
+
 
 router.get("/", (req, res) => {
-    res.render("account/signup")
+    res.render("account/signup-member")
     //sign-up page
 })
 
-router.post("/", (req, res) => {
-    if (req.params.signup == "VOLUNTEER") {
+router.post("/", async (req, res) => {
+    console.log(req.query)
+    if (req.query.signup == "VOLUNTEER") {
+        
         //volunteer signup
-    } else if (req.params.signup == "MEMBER") {
+    } else if (req.query.signup == "MEMBER") {
+        let user =  await models.Member.findOne({
+            where: {email: req.body.email}
+        })
+        console.log(user)
+        if (!user && req.body.password.length > 0) {
+            bcrpyt.hash(req.body.password, 10, (err, encrypted) => {
+                if (err) throw err;
+                models.Member.create({
+                    email:req.body.email,
+                    password:encrypted,
+                    first_name:req.body.first_name,
+                    last_name:req.body.last_name,
+                    phone_number:req.body.phone_number,
+                    address:`${req.body.streetNumber} ${req.body.streetName} ${req.body.city}, ${req.body.state} ${req.body.zipCode}`,
+                    createdAt: new Date(),
+                    updatedAt: new Date()
+                })
+                .then((resolve) => {
+                    res.redirect("/account/login")
+                })
+                .catch((reason) => {
+                    res.send("Error signing you up!")
+                })
+            })
+        } 
         res.send("Member Signed up.")
         //member signup
     } else {
