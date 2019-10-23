9d8a7579622f542e0295fe0568b70efbac688c9f
Kety Cossey <kety.cossey@gmail.com>
2019-10-23 17:18:03 -0500

    login stuff

--- routes/user/login.js
+++ routes/user/login.js
@@ -1,111 +1,36 @@
-const express = require("express");
-const bcrypt = require("bcrypt");
-const router = express.Router();
-// const bodyParser = require("body-parser")
-const models = require('../../models')
-// router.use(bodyParser.urlencoded({ extended: false }));
-//  router.use(bodyParser.json());
- 
+const express = require("express")
+const router = express.Router()
+// const bcrypt = require("bcrypt")
+const models = require("../../models")
+const accountHelper = require("../../helpers/account-helpers")
 
-  //member-login
- 
 router.get("/", (req, res) => {
-   let data = {};
-    if (req.query.registeredSuccessfully) data.registeredSuccessfully= true;
-     console.log (data)
-     if(req.loggedOutSuccessfully) data.loggedOutSuccessfully = true;
-    console.log (req.body.email)
-    res.render('account/login', data);
-  // res.send("I ran!")
-});
+    res.render("account/login.pug", {query:req.query})
+    //member-login
+})
+
+router.post("/", (req, res) => {
+    let userType;
+    if (req.query.login == "MEMBER") userType = "Member";
+    else if (req.query.login == "VOLUNTEER") userType = "Volunteer";
+    else userType = "NOMODEL"
+
+    let loginFailure = () => res.redirect("/account/login?loginFailed=true");
+
+    models[userType].findOne({
+        where:{email:req.body.email}
+    })
+    .then((user) => {
+        if (user) {
+            accountHelper.chkPassword(req.body.password, user, x => {
+                req.session.user = {userType:userType, userId:user.id}
+                res.redirect("/")
+            }, loginFailure)
+        } else loginFailure();
+    })
+    .catch(loginFailure)
 
 
-router.post("/login", async (req, res, next) => {
-    let email = req.body.email,
-    password = req.body.password
-         try {
-        models.Member.findOne({
-            where:{
-                email:email
-            }
-        }) 
-        .then((member) => { 
-            if(member){
-                 bcrypt.compare(req.body.password, member.password, (err, same) =>{
-                 if (err) throw err;
-                 if (!same) throw new Error('Incorrect password');
-                 req.session.user_id = member.id;   
-                });
-        }
-        })} catch (e){
-                // console.log("everything is exploding!!!")
-                // res.send(e);
-            }
-            // next();
-            try {
-                models.Volunteer.findOne({
-                    where:{
-                        email:email
-                    }
-                }) 
-                .then((volunteer) => { 
-                    if(volunteer){
-                         bcrypt.compare(req.body.password, volunteer.password, (err, same) =>{
-                         if (err) throw err;
-                        //  if (!same) throw new Error('Incorrect password');
-
-                        if (same) req.session.user_id
-                         
-                         req.session.user_id = volunteer.id;   
-                        });
-                     } res.redirect("/")
-
-                })} catch (e){
-                        // console.log("everything is exploding!!!")
-                        // res.send(e);
-                    }
-        
-                     
-    });
-
-
-
-
-//                 res.status(500).json({status: 500, message: "email does not exist in our database"
-//             }) } else //{
-//                 // let member = models.Member.build({
-//                 //     email:email,
-//                 //     password:password
-//                 //})
-//                 user.save()
-//             } 
-//             })
-//         })
-//     /*}*/
-// //         bcrypt.compare(req.body.password, dbMember.password, (err, same) =>{
-// //             console.log('we are inside bcrypt')
-//             if (err) throw err;
-//             if (!same) throw new Error('Incorrect password');
-//             req.session.user_id = db.User.id;
-            
-//             res.redirect("/index")
-//         });
-//     } catch (e){
-//         console.log("everything is exploding!!!")
-//         res.send(e);
-//     }
-// });
-
-
-
-
-//     if (req.params.signup == "VOLUNTEER") {
-//         //volunteer signup
-//     } else if (req.params.signup == "MEMBER") {
-//         //member signup
-//     } else {
-//         res.send("An error occurred during login!")
-//     }
-// })
+})
 
 module.exports = router
