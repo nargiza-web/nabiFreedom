0dd2381cf28f780e91c1790ec4caddda0da77984
Nargiza O <nargiza2k@gmail.com>
2019-10-25 12:34:38 -0500

    database thing adding

---
+++ config/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  development: {
+    username: "postgres",
+    password: null,
+    database: "nabiFreedom",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    operatorsAliases: false
+  },
+  test: {
+  username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: "postgres",
+   
+  }
+};
+
+
