ee4f3d9679463954114e20e638fe1fe5e0c15db3
Nargiza O <nargiza2k@gmail.com>
2019-10-25 16:50:17 -0500

    fixed the index.js for database connection

--- config/config.js
+++ config/config.js
@@ -7,14 +7,7 @@ module.exports = {
     dialect: "postgres",
     operatorsAliases: false
   },
-  test: {
-  username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-    operatorsAliases: false
-  },
+  
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
